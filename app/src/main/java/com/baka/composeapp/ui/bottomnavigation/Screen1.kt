d4081ecce25a80488274d0ce3447b39e462f2dcc
H Ngg <hnn@gmail.com>
2024-10-09 22:21:07 +0700

    Some of charts, Canvas (Continue)

--- app/src/main/java/com/baka/composeapp/ui/bottomnavigation/Screen1.kt
+++ app/src/main/java/com/baka/composeapp/ui/bottomnavigation/Screen1.kt
@@ -1,5 +1,6 @@
 package com.baka.composeapp.ui.bottomnavigation
 
+import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
@@ -15,6 +16,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
@@ -22,11 +24,18 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.baka.composeapp.helper.Logger
+import kotlin.math.PI
+import kotlin.math.sin
 
 
 @Composable
@@ -39,215 +48,284 @@ fun Screen1() {
             .verticalScroll(scrollState),
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        Surface(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(24.dp)
-        ) {
-            Canvas(
-                modifier = Modifier.size(200.dp)
-            ) {
-                drawRect(
-                    topLeft = Offset(50f, 50f),
-                    size = Size(300f, 300f),
-                    color = Color.Red,
-                    alpha = 1f,
-                    style = Fill,
-                )
-            }
-        }
-        Surface(
-            onClick = { /*TODO*/ },
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Color.White)
-                .padding(24.dp)
-        ) {
-            Canvas(modifier = Modifier.size(200.dp), onDraw = {
-                drawCircle(Color(0xFFAAFF00))
-            })
-        }
+        DrawRectangle()
         Spacer(modifier = Modifier.padding(8.dp))
-        Surface(
-            onClick = { /*TODO*/ },
-            /* modifier = Modifier
+        DrawCircle()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawPackMan()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawCircleBeveledEdge()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawLine()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawOval1()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawOval2()
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawPath()
+        //https://proandroiddev.com/line-chart-ui-with-jetpack-compose-a-simple-guide-f9b8b80efc83
+        Spacer(modifier = Modifier.padding(8.dp))
+        DrawLines()
+        //https://dev.to/tkuenneth/drawing-and-painting-in-jetpack-compose-1-2okl
+        Spacer(modifier = Modifier.padding(8.dp))
+        SinCosPath()
+    }
+}
+
+@Composable
+private fun DrawOval2() {
+    Surface(onClick = { /*TODO*/ }) {
+        val colorPaint = Color(0xFFBA1122)
+        Canvas(modifier = Modifier
+            .size(200.dp)
+            .padding(16.dp), onDraw = {
+            drawOval(
+                color = colorPaint,
+                topLeft = Offset.Zero,
+                size = Size(size.width - 10f, size.height - 80f)
+            )
+        })
+    }
+}
+
+@Composable
+private fun DrawOval1() {
+    Surface(onClick = { /*TODO*/ }) {
+        Canvas(modifier = Modifier
+            .size(200.dp)
+            .padding(16.dp), onDraw = {
+            drawOval(
+                brush = Brush.linearGradient(listOf(Color.Red, Color.Blue)),
+                topLeft = Offset(10f, 10f),
+                size = Size(size.width - 10f, size.height - 10f)
+            )
+        })
+    }
+}
+
+@Composable
+private fun DrawLine() {
+    Surface(onClick = { /*TODO*/ }) {
+        val colorPaint = Color(0xFFBA6688)
+        Canvas(modifier = Modifier
+            .fillMaxWidth(0.5f)
+            .height(100.dp)
+            .padding(16.dp), onDraw = {
+            drawLine(
+                color = colorPaint,
+                start = Offset.Zero,
+                end = Offset(200f, 50f),
+                strokeWidth = 24f,
+                cap = StrokeCap.Round
+            )
+        })
+    }
+}
+
+@Composable
+private fun DrawCircleBeveledEdge() {
+    Surface(
+        onClick = { /*TODO*/ },
+    ) {
+        val paintColor = Color(0xFF460252)
+        Canvas(modifier = Modifier
+            .size(200.dp)
+            .padding(16.dp), onDraw = {
+            drawArc(paintColor, 0f, 270f, false)
+        })
+    }
+}
+
+@Composable
+private fun DrawPackMan() {
+    Surface(
+        onClick = { /*TODO*/ },
+        /* modifier = Modifier
                  .fillMaxWidth()
                  .fillMaxHeight()
                  .background(Color.LightGray)*/
+    ) {
+        val paintColor = Color(0xFFBA68C8)
+        Canvas(modifier = Modifier
+            .size(200.dp)
+            .padding(16.dp), onDraw = {
+            drawArc(paintColor, 30f, 270f, true)
+        })
+    }
+}
+
+@Composable
+private fun DrawCircle() {
+    Surface(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(24.dp)
+    ) {
+        Canvas(modifier = Modifier.size(200.dp), onDraw = {
+            drawCircle(Color(0xFFAAFF00))
+        })
+    }
+}
+
+@Composable
+private fun DrawRectangle() {
+    Surface(
+        onClick = { /*TODO*/ },
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(24.dp)
+    ) {
+        Canvas(
+            modifier = Modifier.size(200.dp)
         ) {
-            val paintColor = Color(0xFFBA68C8)
-            Canvas(modifier = Modifier
-                .size(200.dp)
-                .padding(16.dp), onDraw = {
-                drawArc(paintColor, 0f, 270f, true)
-            })
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-        Surface(
-            onClick = { /*TODO*/ },
-        ) {
-            val paintColor = Color(0xFF460252)
-            Canvas(modifier = Modifier
-                .size(200.dp)
-                .padding(16.dp), onDraw = {
-                drawArc(paintColor, 0f, 270f, false)
-            })
-        }
-        Spacer(modifier = Modifier.padding(8.dp))
-        Surface(onClick = { /*TODO*/ }) {
-            val colorPaint = Color(0xFFBA6688)
-            Canvas(modifier = Modifier
-                .fillMaxWidth(0.5f)
-                .height(100.dp)
-                .padding(16.dp), onDraw = {
-                drawLine(
-                    color = colorPaint,
-                    start = Offset.Zero,
-                    end = Offset(200f, 50f),
-                    strokeWidth = 24f,
-                    cap = StrokeCap.Round
-                )
-            })
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-        Surface(onClick = { /*TODO*/ }) {
-            Canvas(modifier = Modifier
-                .size(200.dp)
-                .padding(16.dp), onDraw = {
-                drawOval(
-                    brush = Brush.linearGradient(listOf(Color.Red, Color.Blue)),
-                    topLeft = Offset(10f, 10f),
-                    size = Size(size.width - 10f, size.height - 10f)
-                )
-            })
-        }
-        Spacer(modifier = Modifier.padding(8.dp))
-        Surface(onClick = { /*TODO*/ }) {
-            val colorPaint = Color(0xFFBA1122)
-            Canvas(modifier = Modifier
-                .size(200.dp)
-                .padding(16.dp), onDraw = {
-                drawOval(
-                    color = colorPaint,
-                    topLeft = Offset.Zero,
-                    size = Size(size.width - 10f, size.height - 80f)
-                )
-            })
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-        Column(modifier = Modifier.size(300.dp)) {
-            val colorPaint = Color(0xFFBA1122)
-            val path = Path().apply {
-                moveTo(50f, 50f)
-                lineTo(300f, 600f)
-                lineTo(550f, 50f)
-                close()
-            }
-            Canvas(modifier = Modifier.padding(16.dp),
-                onDraw = {
-                    drawPath(
-                        path = path,
-                        color = colorPaint,
-                        style = Stroke(width = 8f, cap = StrokeCap.Round),
-                    )
-                })
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-        Column(modifier = Modifier.size(300.dp)) {
-            /*val linePoints = listOf(
-                Pair(300f, 600f), Pair(400f, 400f),
-                Pair(175f, 200f), Pair(80f, 10f)
-            )*/
-            val path = Path().apply {
-                moveTo(300f, 600f)
-                lineTo(400f, 400f)
-                lineTo(175f, 200f)
-                lineTo(80f, 10f)
-                close()
-            }
-            Canvas(
-                modifier = Modifier
-                    .size(300.dp)
-                    .background(Color(0xFFEFF3F1))
-                    .padding(10.dp)
-            ) {
-                // Draw a rectangle
-                drawRect(color = Color(0xFFAA7800))
-                // Draw a circle
-                drawCircle(color = Color(0xFFAADD00), radius = 200f)
-                // Draw a custom path
-                drawPath(
-                    path = path,
-                    brush = Brush.horizontalGradient(
-                        listOf(
-                            Color.Blue,
-                            Color.Magenta
-                        )
-                    ),
-                    style = Stroke(width = 8f, cap = StrokeCap.Round)
-                )
-            }
-        }
-
-        Spacer(modifier = Modifier.padding(8.dp))
-        Column(
-            modifier = Modifier
-                .size(300.dp)
-                .background(Color.White),
-        ) {
-            //https://proandroiddev.com/line-chart-ui-with-jetpack-compose-a-simple-guide-f9b8b80efc83
-            DrawLineList()
+            drawRect(
+                topLeft = Offset(50f, 50f),
+                size = Size(300f, 300f),
+                color = Color.Red,
+                alpha = 1f,
+                style = Fill,
+            )
         }
     }
 }
 
 @Composable
-fun DrawLineList(
-    list: List<Float> = listOf(5f, 6f, 3f, 1f, 2f, 4f, 3f)
-) {
-    val zipList: List<Pair<Float, Float>> = list.zipWithNext().also {
-        Logger.d("Value of list point: $it")
+private fun DrawPath() {
+    Column(modifier = Modifier.size(300.dp)) {
+        val colorPaint = Color(0xFFBA1122)
+        val path = Path().apply {
+            moveTo(50f, 50f)
+            lineTo(300f, 600f)
+            lineTo(550f, 50f)
+            close()
+        }
+        Canvas(modifier = Modifier.padding(16.dp),
+            onDraw = {
+                drawPath(
+                    path = path,
+                    color = colorPaint,
+                    style = Stroke(width = 8f, cap = StrokeCap.Round),
+                )
+            })
+    }
+}
+
+@Composable
+private fun DrawLines() {
+    val list: List<Float> = listOf(5f, 6f, 3f, 1f, 2f, 4f, 3f)
+    Column(modifier = Modifier.size(300.dp)) {
+        /*val linePoints = listOf(
+                Pair(300f, 600f), Pair(400f, 400f),
+                Pair(175f, 200f), Pair(80f, 10f)
+            )*/
+        val path = Path().apply {
+            moveTo(300f, 600f)
+            lineTo(400f, 400f)
+            lineTo(175f, 200f)
+            lineTo(80f, 10f)
+            close()
+        }
+        Canvas(
+            modifier = Modifier
+                .size(300.dp)
+                .background(Color(0xFFEFF3F1))
+                .padding(10.dp)
+        ) {
+            // Draw a rectangle
+            drawRect(color = Color(0xFFAA7800))
+            // Draw a circle
+            drawCircle(color = Color(0xFFAADD00), radius = 200f)
+            // Draw a custom path
+            drawPath(
+                path = path,
+                brush = Brush.horizontalGradient(
+                    listOf(
+                        Color.Blue,
+                        Color.Magenta
+                    )
+                ),
+                style = Stroke(width = 8f, cap = StrokeCap.Round)
+            )
+        }
     }
 
-    Row {
-        val max = list.max()
-        val min = list.min()
+    Spacer(modifier = Modifier.padding(8.dp))
+    Column(
+        modifier = Modifier
+            .size(300.dp)
+            .background(Color.White),
+    ) {
+        val zipList: List<Pair<Float, Float>> = list.zipWithNext().also {
+            Logger.d("Value of list point: $it")
+        }
 
-        val lineColor =
-            if (list.last() > list.first()) Color(0xFF2596be) else Color(0xFF063970)
-
-        for (pair in zipList) {
-
-            val fromValuePercentage = getValuePercentageForRange(pair.first, max, min)
-            val toValuePercentage = getValuePercentageForRange(pair.second, max, min)
-
+        Row {
+            val max = list.max()
+            val min = list.min()
+            val density = LocalDensity.current
+            val textPaint = remember(density) {
+                Paint().apply {
+                    color = Color(0xFF91085B).toArgb()
+                    textAlign = Paint.Align.CENTER
+                    textSize = density.run { 12.sp.toPx() }
+                }
+            }
             Canvas(
                 modifier = Modifier
                     .fillMaxHeight()
                     .weight(1f),
                 onDraw = {
-                    val fromPoint =
-                        Offset(x = 0f, y = size.height.times(1 - fromValuePercentage))
-                    val toPoint =
-                        Offset(x = size.width, y = size.height.times(1 - toValuePercentage))
+                    val yAxisSpace = size.height / list.size
+                    val paddingSpace = 16
+                    for (i in 0 until 5) {
+                        drawContext.canvas.nativeCanvas.drawText(
+                            "$i",
+                            paddingSpace.dp.toPx() / 2f,
+                            size.height - yAxisSpace * (i + 1),
+                            textPaint
+                        )
+                        drawLine(
+                            color = Color(0xFF91085B),
+                            start = Offset(0 + paddingSpace.div(2f), size.height - paddingSpace),
+                            end = Offset(
+                                paddingSpace.div(2f),
+                                size.height - yAxisSpace * (i + 1)
+                            ),
+                            strokeWidth = 24f,
+                            cap = StrokeCap.Round
+                        )
+                    }
 
-                    drawLine(
-                        color = lineColor,
-                        start = fromPoint,
-                        end = toPoint,
-                        strokeWidth = 3f,
-                        cap = StrokeCap.Round,
-                    )
                 })
         }
     }
 }
 
-private fun getValuePercentageForRange(value: Float, max: Float, min: Float) =
-    (value - min) / (max - min)
+@Composable
+private fun SinCosPath() {
+    Canvas(modifier = Modifier
+        .size(300.dp)
+        .padding(16.dp)
+        .background(Color.White),
+        onDraw = {
+            val middleW = size.width / 2
+            val middleH = size.height / 2
+            drawLine(Color.Gray, Offset(0f, middleH), Offset(size.width - 1, middleH))
+            drawLine(Color.Gray, Offset(middleW, 0f), Offset(middleW, size.height - 1))
+            val points = mutableListOf<Offset>()
+            for (x in 0 until size.width.toInt()) {
+                val y = (sin(x * (2f * PI / size.width)) * middleH + middleH).toFloat()
+                points.add(Offset(x.toFloat(), y))
+            }
+            drawPoints(
+                points = points,
+                strokeWidth = 4f,
+                pointMode = PointMode.Points,
+                color = Color.Blue
+            )
+        }
+    )
+}
--- app/src/main/java/com/baka/composeapp/ui/bottomnavigation/Screen2.kt
+++ app/src/main/java/com/baka/composeapp/ui/bottomnavigation/Screen2.kt
@@ -1,23 +1,41 @@
 package com.baka.composeapp.ui.bottomnavigation
 
+import android.graphics.Paint
+import android.graphics.PointF
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asAndroidPath
+import androidx.compose.ui.graphics.asComposePath
 import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.baka.composeapp.ui.models.GraphAppearance
 
 @Composable
 fun Screen2() {
@@ -40,6 +58,50 @@ fun Screen2() {
                 .padding(24.dp)
         ) {
             BarChart()
+        }
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color.White)
+                .padding(24.dp)
+        ) {
+            val yStep = 50
+            /* to test with random points */
+            /*val points = (0..9).map {
+                var num = Random.nextInt(350)
+                if (num <= 50)
+                    num += 100
+                num.toFloat()
+            }*/
+            /* to test with fixed points */
+            val points = listOf(150f, 100f, 250f, 200f, 330f, 300f, 90f, 120f, 285f, 199f)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.DarkGray)
+            ) {
+                Graph(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(500.dp),
+                    xValues = (0..9).map { it + 1 },
+                    yValues = (0..6).map { (it + 1) * yStep },
+                    points = points,
+                    paddingSpace = 16.dp,
+                    verticalStep = yStep,
+                    graphAppearance = GraphAppearance(
+                        graphColor = Color.White,
+                        graphAxisColor = MaterialTheme.colorScheme.primary,
+                        graphThickness = 1f,
+                        isColorAreaUnderChart = true,
+                        colorAreaUnderChart = Color.Green,
+                        isCircleVisible = true,
+                        circleColor = MaterialTheme.colorScheme.secondary,
+                        backgroundColor = MaterialTheme.colorScheme.background
+                    )
+                )
+            }
         }
     }
 }
@@ -76,5 +138,130 @@ fun BarChart() {
                 )
             }
         }
+    }
+}
+
+@Composable
+fun Graph(
+    modifier: Modifier,
+    xValues: List<Int>,
+    yValues: List<Int>,
+    points: List<Float>,
+    paddingSpace: Dp,
+    verticalStep: Int,
+    graphAppearance: GraphAppearance,
+) {
+    val controlPoints1 = mutableListOf<PointF>()
+    val controlPoints2 = mutableListOf<PointF>()
+    val coordinates = mutableListOf<PointF>()
+    val density = LocalDensity.current
+    val textPaint = remember(density) {
+        Paint().apply {
+            color = graphAppearance.graphAxisColor.toArgb()
+            textAlign = Paint.Align.CENTER
+            textSize = density.run { 12.sp.toPx() }
+        }
+    }
+    Box(
+        modifier = modifier
+            .background(graphAppearance.backgroundColor)
+            .padding(horizontal = 8.dp, vertical = 12.dp),
+        contentAlignment = Center
+    ) {
+        Canvas(
+            modifier = Modifier.fillMaxSize(),
+            onDraw = {
+                val xAxisSpace = (size.width - paddingSpace.toPx()) / xValues.size
+                val yAxisSpace = size.height / yValues.size
+                //val yAxisSpace = size.height / yValues.size
+                /** placing x axis points */
+                for (i in xValues.indices) {
+                    drawContext.canvas.nativeCanvas.drawText(
+                        "${xValues[i]}",
+                        xAxisSpace * (i + 1),
+                        size.height - 30,
+                        textPaint
+                    )
+                }
+                /** placing y axis points */
+                for (i in yValues.indices) {
+                    drawContext.canvas.nativeCanvas.drawText(
+                        "${yValues[i]}",
+                        paddingSpace.toPx() / 2f,
+                        size.height - yAxisSpace * (i + 1),
+                        textPaint
+                    )
+                }
+                /** placing our x axis points */
+                for (i in points.indices) {
+                    val x1 = xAxisSpace * xValues[i]
+                    val y1 = size.height - (yAxisSpace * (points[i] / verticalStep.toFloat()))
+                    coordinates.add(PointF(x1, y1))
+                    /** drawing circles to indicate all the points */
+                    if (graphAppearance.isCircleVisible) {
+                        drawCircle(
+                            color = graphAppearance.circleColor,
+                            radius = 10f,
+                            center = Offset(x1, y1)
+                        )
+                    }
+                }
+                /** calculating the connection points */
+                for (i in 1 until coordinates.size) {
+                    controlPoints1.add(
+                        PointF(
+                            (coordinates[i].x + coordinates[i - 1].x) / 2,
+                            coordinates[i - 1].y
+                        )
+                    )
+                    controlPoints2.add(
+                        PointF(
+                            (coordinates[i].x + coordinates[i - 1].x) / 2,
+                            coordinates[i].y
+                        )
+                    )
+                }
+                /** drawing the path */
+                val stroke = Path().apply {
+                    reset()
+                    moveTo(coordinates.first().x, coordinates.first().y)
+                    for (i in 0 until coordinates.size - 1) {
+                        cubicTo(
+                            controlPoints1[i].x, controlPoints1[i].y,
+                            controlPoints2[i].x, controlPoints2[i].y,
+                            coordinates[i + 1].x, coordinates[i + 1].y
+                        )
+                    }
+                }
+
+                /** filling the area under the path */
+                val fillPath = android.graphics.Path(stroke.asAndroidPath())
+                    .asComposePath()
+                    .apply {
+                        lineTo(xAxisSpace * xValues.last(), size.height - yAxisSpace)
+                        lineTo(xAxisSpace, size.height - yAxisSpace)
+                        close()
+                    }
+                if (graphAppearance.isColorAreaUnderChart) {
+                    drawPath(
+                        path = fillPath,
+                        brush = Brush.verticalGradient(
+                            listOf(
+                                graphAppearance.colorAreaUnderChart,
+                                Color.Transparent,
+                            ),
+                            endY = size.height - yAxisSpace,
+                        ),
+                    )
+                }
+                drawPath(
+                    stroke,
+                    color = graphAppearance.graphColor,
+                    style = Stroke(
+                        width = graphAppearance.graphThickness,
+                        cap = StrokeCap.Round
+                    )
+                )
+            })
     }
 }
